a4629021efd52a3605942fc0fb9281cdcc3a587b
Dan Kashkovsky <dannie.k@me.com>
2016-05-16 16:30:36 +0300

    Mobile app

---
+++ XChat/ChatPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XChat
+{
+	public partial class ChatPage : ContentPage
+	{
+		private ChatViewModel _model;
+		public ChatPage(string name, int color)
+		{
+			_model = new ChatViewModel(name, color);
+			BindingContext = _model;
+			InitializeComponent();
+
+			messageEntry.Completed += (sender, e) =>
+			{
+				sendButton.Command.Execute(null);
+			};
+		}
+
+		public void HandleFocused(object sender, EventArgs e)
+		{
+			mainGrid.RowDefinitions[2].Height = new GridLength(5, GridUnitType.Star);
+			sendButton.IsVisible = false;
+		}
+		public void HandleUnfocused(object sender, EventArgs e)
+		{
+			mainGrid.RowDefinitions[2].Height = 0;
+			sendButton.IsVisible = true;
+		}
+	}
+}
+
---
+++ XChat/MessageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XChat
+{
+	public class MessageModel
+	{
+		public string Name { get; set; }
+		public string Message { get; set; }
+		public DateTime PostedOn { get; set; }
+		public string Color { get; set; }
+		public Thickness Pad { get; set; }
+	}
+}
+
