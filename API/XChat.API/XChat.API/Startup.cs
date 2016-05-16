9bbbe2bffe6878fbadc6a3f7342ca264325e919e
Dan Kashkovsky <dannie.k@me.com>
2016-05-16 21:10:37 +0300

    API project

---
+++ API/XChat.API/XChat.API/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(XChat.API.Startup))]
+
+namespace XChat.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
